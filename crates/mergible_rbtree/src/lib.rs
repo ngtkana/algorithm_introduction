402ee9b58cca7e7ac27417d6e0e8c7c33a300d99
ngtkana <ngtkana@gmail.com>
2020-11-08 19:16:53 +0900

    Add rb_insert to mergible_rbtreee

--- crates/mergible_rbtree/src/lib.rs
+++ crates/mergible_rbtree/src/lib.rs
@@ -1,5 +1,9 @@
+mod color;
 mod paren;
+pub mod validate;
 
+use color::Color;
+use dbg::{lg, msg};
 use std::{cmp::Ordering, fmt::Debug};
 
 pub struct RBTree<K, V>(BoxedNode<K, V>);
@@ -9,6 +13,7 @@ impl<K: Ord + Debug, V: Debug> RBTree<K, V> {
     }
     pub fn insert(&mut self, k: K, v: V) {
         self.0.insert(k, v);
+        self.0.set_color(Color::Black);
     }
     pub fn delete(&mut self, k: K) {
         self.0.delete(k);
@@ -31,6 +36,7 @@ impl<K: Ord + Debug, V: Debug> BoxedNode<K, V> {
             child: [Self(None), Self(None)],
             key: k,
             value: v,
+            color: Color::Red,
         })))
     }
     fn is_nil(&self) -> bool {
@@ -39,6 +45,9 @@ impl<K: Ord + Debug, V: Debug> BoxedNode<K, V> {
 
     fn replace(&mut self, x: Self) -> Self {
         std::mem::replace(self, x)
+    }
+    fn take(&mut self) -> Self {
+        self.replace(Self(None))
     }
 
     // unwrap
@@ -67,14 +76,93 @@ impl<K: Ord + Debug, V: Debug> BoxedNode<K, V> {
     fn take_child(&mut self, i: usize) -> Self {
         self.child_mut(i).replace(Self(None))
     }
+    fn set_color(&mut self, color: Color) {
+        self.unwrap_mut().color = color;
+    }
+    fn swap_color(&mut self, y: &mut Self) {
+        let x = self.unwrap_mut();
+        let y = y.unwrap_mut();
+        std::mem::swap(&mut x.color, &mut y.color);
+    }
+    fn swap_color_with_child(&mut self, i: usize) {
+        let color = self.color();
+        let color = std::mem::replace(&mut self.child_mut(i).unwrap_mut().color, color);
+        self.unwrap_mut().color = color;
+    }
+    fn rotate(&mut self, i: usize) {
+        let mut x = self.take();
+        let mut y = x.take_child(i);
+        let z = y.take_child(1 - i);
+        x.replace_empty_child(i, z);
+        y.replace_empty_child(1 - i, x);
+        *self = y
+    }
+    fn swap_color_rotate(&mut self, i: usize) {
+        self.swap_color_with_child(i);
+        self.rotate(i);
+    }
+
+    // color
+    fn color(&self) -> Color {
+        self.0.as_ref().map_or(Color::Black, |x| x.color)
+    }
+    fn is_red(&self) -> bool {
+        self.color() == Color::Red
+    }
+    fn is_black(&self) -> bool {
+        self.color() == Color::Black
+    }
+    fn assert_red(&self) -> &Self {
+        assert!(self.is_red());
+        self
+    }
+    fn assert_black(&self) -> &Self {
+        assert!(self.is_black());
+        self
+    }
 
     // rb ops
-    fn insert(&mut self, k: K, v: V) {
+    fn insert(&mut self, k: K, v: V) -> Option<DoubleRed> {
         if let Some(me) = self.0.as_mut() {
             let i = if k <= me.key { 0 } else { 1 };
-            me.child[i].insert(k, v);
+            let e = me.child[i].insert(k, v);
+            self.insert_and_then(i, e)
         } else {
             *self = Self::new(k, v);
+            Some(DoubleRed::Me)
+        }
+    }
+    fn insert_and_then(&mut self, i: usize, e: Option<DoubleRed>) -> Option<DoubleRed> {
+        match e? {
+            DoubleRed::Me => match self.color() {
+                Color::Red => Some(DoubleRed::Child(i)),
+                Color::Black => None,
+            },
+            DoubleRed::Child(j) => self.insert_fixup(i, j),
+        }
+    }
+    fn insert_fixup(&mut self, i: usize, j: usize) -> Option<DoubleRed> {
+        msg!("insert_fixup", (&self, i, j));
+        self.assert_black()
+            .child(i)
+            .assert_red()
+            .child(j)
+            .assert_red();
+        match self.child(1 - i).color() {
+            Color::Red => {
+                (0..2).for_each(|i| self.child_mut(i).set_color(Color::Black));
+                self.set_color(Color::Red);
+                Some(DoubleRed::Me)
+            }
+            Color::Black => {
+                if i == j {
+                    self.swap_color_rotate(i);
+                    None
+                } else {
+                    self.child_mut(i).swap_color_rotate(j);
+                    self.insert_fixup(i, 1 - j)
+                }
+            }
         }
     }
     fn delete(&mut self, k: K) -> Option<BoxedNode<K, V>> {
@@ -128,11 +216,17 @@ struct Node<K, V> {
     child: [BoxedNode<K, V>; 2],
     key: K,
     value: V,
+    color: Color,
+}
+
+enum DoubleRed {
+    Me,
+    Child(usize),
 }
 
 #[cfg(test)]
 mod tests {
-    use super::RBTree;
+    use super::{validate, RBTree};
     use rand::prelude::*;
 
     #[test]
@@ -164,9 +258,9 @@ mod tests {
         for _ in 0..t {
             let mut test = Test::new();
             for _ in 0..q {
-                match rng.gen_range(0, 2) {
+                match rng.gen_range(0, 1) {
                     0 => test.insert(rng.gen_range(0, 10)),
-                    1 => test.delete(rng.gen_range(0, 10)),
+                    // 1 => test.delete(rng.gen_range(0, 10)),
                     _ => unreachable!(),
                 }
             }
@@ -207,6 +301,7 @@ mod tests {
         }
         fn postprocess(&self) {
             println!("rbt = {:?}", &self.rbt);
+            validate::all(&self.rbt);
             assert_eq!(
                 &self
                     .rbt
